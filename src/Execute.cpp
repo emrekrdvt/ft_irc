2d34aca2f5f4266a41182d4a854b7c5add5e6041
Emre <karadavut.emre@hotmail.com>
2023-02-13 20:25:53 +0300

    notice update.

--- src/Execute.cpp
+++ src/Execute.cpp
@@ -13,7 +13,7 @@ Execute::Execute(){
 	this->commands.push_back(Command("USER", &Execute::user));
 	this->commands.push_back(Command("CAP", &Execute::cap));
 	//this->commands.push_back(Command("WHO", &Execute::who));
-	//this->commands.push_back(Command("NOTICE", &Execute::notice));
+	this->commands.push_back(Command("NOTICE", &Execute::notice));
 	//this->commands.push_back(Command("MODE", &Execute::mode));
 	//this->commands.push_back(Command("LIST", &Execute::list));
 	//this->commands.push_back(Command("TOPIC", &Execute::topic));
@@ -168,9 +168,29 @@ void Execute::who(int &fd, Server *server, std::string message){
 }
 
 void Execute::notice(int &fd, Server *server, std::string message){
-	(void)message;
-	(void)server;
-	(void)fd;
+	std::string toWho = message.substr(0, message.find(" "));
+	std::string msg = message.substr(message.find(" ") + 1);
+	std::cout << toWho << std::endl;
+	std::cout << msg << std::endl;
+
+	int recvFd = server->getUser(toWho)->getFd();
+
+	std::vector<Channel *> channels = server->getChannels();
+	std::vector<Channel *>::iterator it = channels.begin();
+	for (; it != channels.end(); it++)
+	{
+		std::vector<User *> users = (*it)->getUsers();
+		std::vector<User *>::iterator it2 = users.begin();
+		for (; it2 != users.end(); it2++)
+		{
+			if ((*it2)->getFd() == fd)
+			{
+				std::string channelName = (*it)->getName();
+				std::string nickname = (*it2)->getNickname();
+				server->sender(recvFd, utils::getPrefix(server->getUser(fd)) + " NOTICE " + channelName + " :" + msg);
+			}
+		}
+	}
 }
 
 void Execute::mode(int &fd, Server *server, std::string message){
